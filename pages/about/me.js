3e4a0d0e24d755b170082b16d0522ce878f74ec5
Shashank Dubey <jfriday464@gmail.com>
2021-09-23 15:39:51 +0530

    New changes

--- pages/about/me.js
+++ pages/about/me.js
@@ -1,7 +1,13 @@
+import { Box } from "@material-ui/core";
+import SideNavbar from "../../components/SideNavbar";
+
 const Me = () => {
    return (
       <div>
-         <h1>This is Me</h1>
+         <SideNavbar />
+         <Box mt={10} ml={37}>
+            <h1>This is Me. How you doing</h1>
+         </Box>
       </div>
    );
 };
